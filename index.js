91625d95d8a97dfdad4840cf5ac42077868b7fc6
Emranhossainshuvo <shuvoorba28@gmail.com>
2023-12-22 18:05:38 +0600

    deployed to vercel

--- index.js
+++ index.js
@@ -24,15 +24,15 @@ let taskCollection;
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     taskCollection = client.db("Taskly").collection("task-collection");
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
